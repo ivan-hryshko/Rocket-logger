676e45361269a4609fe7ac48ef881b840899b2bf
Saloid <svetotled@gmail.com>
2019-06-22 19:49:57 +0300

    DFU in settings enabled (update via USB)

--- Soft/Embedded/Src/STM-logger/src/main.cpp
+++ Soft/Embedded/Src/STM-logger/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include "pins.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	pinMode(PARACHUTE_LAUNCH_FUSE_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	digitalWrite(PARACHUTE_LAUNCH_FUSE_PIN, HIGH);
+	delay(1000);
+	digitalWrite(PARACHUTE_LAUNCH_FUSE_PIN, LOW);
+	delay(1000);
 }
---
+++ Soft/Embedded/Src/STM-logger/src/pins.h
@@ -0,0 +1,3 @@
+#define ENGINE_START_FUSE_PIN     PB5
+#define PARACHUTE_LAUNCH_FUSE_PIN PB7
+#define BRIGHT_LED_PIN            PB6
