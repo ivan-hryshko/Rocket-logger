40a8838e6538d279a9b4c0f1e2590865cc5bb010
Saloid <svetotled@gmail.com>
2019-11-15 20:10:04 +0200

    Demo works

--- Soft/Embedded/NRF24_receiver/src/main.cpp
+++ Soft/Embedded/NRF24_receiver/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
+Serial.println("Start");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.println("Acc:" + String(random(0, 23000)/1000.0));
+  Serial.println("Hgh:" + String(random(0, 1000)));
+  Serial.println("Par:" + String(random(0, 2)));
+  Serial.println("Ang:" + String(random(0, 18000)/100.0));
+  Serial.println("---------------------");
+
+  delay(1000);
 }
