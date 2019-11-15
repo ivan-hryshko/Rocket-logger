dec6300d6b66c685ea67ecc0b6f3f2adf0614191
Saloid <svetotled@gmail.com>
2019-11-15 20:38:27 +0200

    JSON format

--- Soft/Embedded/NRF24_receiver/src/main.cpp
+++ Soft/Embedded/NRF24_receiver/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
 void setup()
 {
   Serial.begin(115200);
-Serial.println("Start");
+  Serial.println("Start");
 }
 
 void loop()
 {
-  Serial.println("Acc:" + String(random(0, 23000)/1000.0));
-  Serial.println("Hgh:" + String(random(0, 1000)));
-  Serial.println("Par:" + String(random(0, 2)));
-  Serial.println("Ang:" + String(random(0, 18000)/100.0));
-  Serial.println("---------------------");
+  StaticJsonDocument<200> json;
+
+  json["Acc"] = random(0, 23000)/1000.0;
+  json["Hgh"] = random(0, 1000);
+  json["Par"] = random(0, 2);
+  json["Ang"] = random(0, 18000) / 100.0;
+
+  serializeJson(json, Serial);
+  Serial.print('\n');
 
   delay(1000);
 }
