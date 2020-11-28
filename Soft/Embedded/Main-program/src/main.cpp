84f3efce2fff75f52bd8eaf4ed1d70a3772ac88f
Saloid <svetotled@gmail.com>
2020-11-28 23:53:17 +0200

    Main refactored, but it still fails after some time

--- Soft/Embedded/Main-program/src/main.cpp
+++ Soft/Embedded/Main-program/src/main.cpp
@@ -11,56 +11,61 @@
 #include "sd_log.h"
 #include "MPU9250.h"
 #include "BMP280_DEV.h"
+#include "ArduinoLog.h"
 #include <string.h>
 #include <Wire.h>
 
-#define MAIN_PERIOD 2000
+#define MAIN_LOG_LEVEL LOG_LEVEL_VERBOSE
+
+#define MAIN_PERIOD 1000
 
 BMP280_DEV bmp; // use I2C interface
 MPU9250 MPU(Wire, 0x68);
+Logging main_log;
 
 void setup()
 {
+    // Wire.setSDA(PB11);
+    // Wire.setSCL(PB10);
     Serial.begin(921600); // Open serial communications and wait for port to open:
+    main_log.begin(MAIN_LOG_LEVEL, &Serial);
+    main_log.notice("Waiting for monitor open\n");
     while (!Serial)
     {
         ; // wait for serial port to connect.
     }
-    Serial.println("Logger start");
-    Wire.setSDA(PB11);
-    Wire.setSCL(PB10);
-    Wire.begin();
-    Wire.setClock(400000);
-    // int status = MPU.begin();
-    // if (status < 0)
-    // {
-    //     Serial.print("MPU init fail: ");
-    //     Serial.println(status);
-    //     while (1)
-    //     {
-    //     }
-    // }
-    // else
-    // {
-    //     Serial.println("MPU inited");
-    // }
-    if (bmp.begin(NORMAL_MODE, OVERSAMPLING_X1, OVERSAMPLING_X1, IIR_FILTER_OFF) == 0)
+    main_log.notice("Logger start\n");
+    Wire.begin((uint8_t)PB11, (uint8_t)PB10);
+    int status = MPU.begin();
+    if (status < 0)
     {
-        Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+        Serial.print("MPU init fail: ");
+        Serial.println(status);
         while (1)
         {
         }
     }
     else
     {
-        // if (bmp.isBME280())
-        // {
-        //     Serial.println("sensor is a BME280");
-        // }
-        // else
-        // {
-        //     Serial.println("sensor is a BMP280");
-        // }
+        MPU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+        MPU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
+        MPU.setSrd(0);
+        main_log.notice("MPU inited\n");
+    }
+    if (bmp.begin(NORMAL_MODE, BMP280_I2C_ADDR) == 0)
+    {
+        main_log.fatal(F("Could not find a valid BMP280 sensor, check wiring!"));
+        while (1)
+        {
+        }
+    }
+    else
+    {
+        bmp.setPresOversampling(OVERSAMPLING_X1);
+        bmp.setTempOversampling(OVERSAMPLING_X1);
+        bmp.setIIRFilter(IIR_FILTER_OFF);
+        bmp.setTimeStandby(TIME_STANDBY_05MS);
+        Wire.setClock(400000);
 
         //reset sensor to default parameters.
         // bmp280.startNormalConversion();
@@ -80,16 +85,8 @@ void setup()
         //     Serial.println("could not start measurement, is a measurement already running?");
         //     return;
         // }
-        // bmp.setPresOversampling(OVERSAMPLING_SKIP);    // Set the pressure oversampling to X4
-        // bmp.setTempOversampling(OVERSAMPLING_SKIP); // Set the temperature oversampling to X1
-        // bmp.setIIRFilter(IIR_FILTER_OFF);
-        // bmp.setTimeStandby(TIME_STANDBY_05MS);
-        Serial.println("BMP inited");
+        main_log.notice("BMP inited\n");
     }
-    // bmp.startNormalConversion();
-    // MPU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
-    // MPU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
-    // MPU.setSrd(0);
     // MPU.enableDataReadyInterrupt();
 
     analogReadResolution(8);
@@ -102,7 +99,7 @@ void setup()
     // MPU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
 
     // MPU.setSrd(19);
-    Serial.println("init done");
+    main_log.notice("init done\n");
 }
 
 typedef struct __attribute__((packed))
@@ -110,130 +107,92 @@ typedef struct __attribute__((packed))
     uint8_t num;          // 1 byte
     uint32_t micros;      // 4 bytes
     uint8_t batt_voltage; // 1 byte
-    uint8_t flags;        // 1 byte
+    struct
+    {
+        bool bmp_data:1;
+        bool mpu_data:1;
+    } __attribute__((packed)) flags;        // 1 byte
 
-    uint16_t bmp_temp;  // 2 bytes
-    uint16_t bmp_press; // 2 bytes
+    struct
+    {
+        uint16_t temp;  // 2 bytes
+        uint16_t press; // 2 bytes
+    } __attribute__((packed)) bmp;
+    struct
+    {
+        int16_t temp; // 2 bytes
 
-    int16_t mpu_temp; // 2 bytes
+        int16_t acc_x; // 2 bytes
+        int16_t acc_y; // 2 bytes
+        int16_t acc_z; // 2 bytes
 
-    int16_t mpu_acc_x; // 2 bytes
-    int16_t mpu_acc_y; // 2 bytes
-    int16_t mpu_acc_z; // 2 bytes
+        int16_t gyr_x; // 2 bytes
+        int16_t gyr_y; // 2 bytes
+        int16_t gyr_z; // 2 bytes
 
-    int16_t mpu_gyr_x; // 2 bytes
-    int16_t mpu_gyr_y; // 2 bytes
-    int16_t mpu_gyr_z; // 2 bytes
-
-    int16_t mpu_mag_x; // 2 bytes
-    int16_t mpu_mag_y; // 2 bytes
-    int16_t mpu_mag_z; // 2 bytes
+        int16_t mag_x; // 2 bytes
+        int16_t mag_y; // 2 bytes
+        int16_t mag_z; // 2 bytes
+    } __attribute__((packed)) mpu;
     uint8_t reserved[1];
 
 } measured_values_t;
 
-typedef struct __attribute__((packed))
-{
-    uint8_t buff[32];
-} data_to_write_struct;
-
-data_to_write_struct data;
 
 void loop()
 {
-
-    // while (true)
-    // {
-    //     uint32_t curr_time = micros();
-    //     static uint32_t prev_time = curr_time - 1000;
-    //     if (curr_time - prev_time >= 1000)
-    //     {
-    //         float temperature, pressure;
-    //         prev_time += 1000;
-    //         // float bmp_temp = 0, bmp_pressure = 0;
-    //         if (bmp.getTempPres(temperature, pressure))
-    //         {
-    //             Serial.print(temperature);
-    //             Serial.print(" ");
-    //             Serial.println(pressure);
-    //         }
-    //         else
-    //         {
-    //             Serial.println("BMP not ready");
-    //         }
-    //     }
-    //     //delay(100);
-    // }
-
     static SD_log SD;
     static uint8_t measure_num = 0;
 
-    uint32_t curr_time = micros();
-    static uint32_t last_measure_time = curr_time - MAIN_PERIOD;
+    uint32_t start_time = micros();
+    static uint32_t last_measure_time = start_time - MAIN_PERIOD;
 
-    if (curr_time - last_measure_time >= MAIN_PERIOD)
+    if (start_time - last_measure_time >= MAIN_PERIOD)
     {
+        measured_values_t current_meas;
         last_measure_time += MAIN_PERIOD;
 
-        measured_values_t current_meas =
-            {
-                .num = measure_num++,
-                .micros = curr_time,
-                .batt_voltage = 0, //static_cast<uint8_t>(analogRead(PC0)),
-                .flags = 0,
+        current_meas.num = measure_num++;
+        current_meas.micros = start_time;
 
-                .bmp_temp = 0,
-                .bmp_press = 0,
-
-                .mpu_temp = 0,
-                .mpu_acc_x = 0,
-                .mpu_acc_y = 0,
-                .mpu_acc_z = 0,
-                .mpu_gyr_x = 0,
-                .mpu_gyr_y = 0,
-                .mpu_gyr_z = 0,
-                .mpu_mag_x = 0,
-                .mpu_mag_y = 0,
-                .mpu_mag_z = 0,
-
-                .reserved = {0},
-
-            };
         if (MPU.readSensor() > 0)
         {
-            current_meas.mpu_temp = MPU._tcounts;
+            current_meas.mpu.temp = MPU._tcounts;
 
-            current_meas.mpu_acc_x = MPU._axcounts;
-            current_meas.mpu_acc_y = MPU._aycounts;
-            current_meas.mpu_acc_z = MPU._azcounts;
+            current_meas.mpu.acc_x = MPU._axcounts;
+            current_meas.mpu.acc_y = MPU._aycounts;
+            current_meas.mpu.acc_z = MPU._azcounts;
 
-            current_meas.mpu_gyr_x = MPU._gxcounts;
-            current_meas.mpu_gyr_y = MPU._gycounts;
-            current_meas.mpu_gyr_z = MPU._gzcounts;
+            current_meas.mpu.gyr_x = MPU._gxcounts;
+            current_meas.mpu.gyr_y = MPU._gycounts;
+            current_meas.mpu.gyr_z = MPU._gzcounts;
 
-            current_meas.mpu_mag_x = MPU._hxcounts;
-            current_meas.mpu_mag_y = MPU._hycounts;
-            current_meas.mpu_mag_z = MPU._hzcounts;
+            current_meas.mpu.mag_x = MPU._hxcounts;
+            current_meas.mpu.mag_y = MPU._hycounts;
+            current_meas.mpu.mag_z = MPU._hzcounts;
+
+            current_meas.flags.mpu_data = true;
         }
         else
         {
-            Serial.println("MPU not ready");
+            current_meas.flags.mpu_data = false;
         }
-        
-        Serial.println(current_meas.mpu_acc_z);
+        uint32_t mpu_time = micros();
         float bmp_temp = 0, bmp_pressure = 0;
         if (bmp.getTempPres(bmp_temp, bmp_pressure))
         {
-            Serial.print(bmp_temp);
-            Serial.print(" ");
-            Serial.println(bmp_pressure);
-            current_meas.bmp_temp = bmp_temp*10;
-            current_meas.bmp_press = bmp_pressure*100;
+            // Serial.print(bmp_temp);
+            // Serial.print(" ");
+            // Serial.println(bmp_pressure);
+            current_meas.bmp.temp = bmp_temp*10;
+            current_meas.bmp.press = bmp_pressure*10;
+            current_meas.flags.bmp_data = true;
         }
         else
         {
-            Serial.println("BMP not ready");
+            current_meas.flags.bmp_data = false;
         }
+        uint32_t bmp_time = micros();
         // for (uint8_t j = 0; j < sizeof(current_meas); j++)
         // {
         //     Serial.print(((uint8_t *)(&current_meas))[j], HEX);
@@ -241,6 +200,7 @@ void loop()
         // }
         // Serial.println();
         SD.write(&current_meas, sizeof(current_meas));
-        // Serial.println(micros() - curr_time);
+        uint32_t sd_write_time = micros();
+        main_log.trace("mpu:%dus\tpres:%dus\tsd:%dus\ttotal:%dus\n", (mpu_time - start_time), (bmp_time - mpu_time), (sd_write_time - bmp_time), (sd_write_time - start_time));
     }
 }
