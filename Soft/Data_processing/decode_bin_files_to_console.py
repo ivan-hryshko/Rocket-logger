1787fda764bae769ecb70c299fe45b606a540b4e
SkirriLEX <kirk.andri@gmail.com>
2020-12-08 00:20:32 +0200

    separate data map setups

--- Soft/Data_processing/decode_bin_files_to_console.py
+++ Soft/Data_processing/decode_bin_files_to_console.py
@@ -1,29 +1,7 @@
 from os import listdir
+from data_map import BUFF_SETUP, MAX_BUFF_IN_PACK
 from os.path import isfile, join
 import sys
-
-BUFF_SETUP=[
-#   size  +   *   sign   name
-    [1,   0,  1,  False, "num"],
-    [4,   0,  1,  False, "micros"],
-    [1,   0,  1,  False, "batt_voltage"],
-    [1,   0,  1,  False, "flags"],
-    [2,   0,  1,  False, "bmp_temp"],
-    [2,   0,  1,  False, "bmp_press"],
-    [2,   0,  1,  True,  "mpu_temp"],
-    [2,   0,  1,  True,  "mpu_acc_x"],
-    [2,   0,  1,  True,  "mpu_acc_y"],
-    [2,   0,  1,  True,  "mpu_acc_z"],
-    [2,   0,  1,  True,  "mpu_gyr_x"],
-    [2,   0,  1,  True,  "mpu_gyr_y"],
-    [2,   0,  1,  True,  "mpu_gyr_z"],
-    [2,   0,  1,  True,  "mpu_mag_x"],
-    [2,   0,  1,  True,  "mpu_mag_y"],
-    [2,   0,  1,  True,  "mpu_mag_z"],
-    [1,   0,  1,  True,  "reserved"]
-]
-MAX_BUFF_IN_PACK = len(BUFF_SETUP)
-
 
 buff = [None] * MAX_BUFF_IN_PACK
 def print_buff_console ():
